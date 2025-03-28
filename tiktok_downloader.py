2b12f4fb104af5ab9534c7ee2d4031874066e61b
yqq <youngqqcn@gmail.com>
2025-03-28 19:26:36 +0800

    更新TikTok视频下载流程

--- tiktok_downloader.py
+++ tiktok_downloader.py
@@ -1,10 +1,12 @@
 import asyncio
 import json
+from multiprocessing import Pool, cpu_count
 import os
 import random
 import sys
 import traceback
 from types import CoroutineType
+from typing import List
 
 import aiofiles
 import httpx
@@ -53,6 +55,9 @@ service = webdriver.ChromeService(executable_path="chromedriver-linux64/chromedr
 driver = webdriver.Chrome(service=service, options=options)
 
 
+url_sets = set([])
+
+
 async def fetch_data(url: str, headers: dict = None):
     headers = (
         {
@@ -68,7 +73,39 @@ async def fetch_data(url: str, headers: dict = None):
 
 
 # 下载视频专用
-async def fetch_data_stream(url: str, headers: dict = None, file_path: str = None):
+# async def fetch_data_stream(url: str, headers: dict = None, file_path: str = None):
+#     headers = (
+#         {
+#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+#         }
+#         if headers is None
+#         else headers.get("headers")
+#     )
+#     async with httpx.AsyncClient() as client:
+#         # 启用流式请求
+#         print(f"流式下载url: {url}")
+#         async with client.stream("GET", url, headers=headers) as response:
+#             response.raise_for_status()
+
+#             # 流式保存文件
+#             async with aiofiles.open(file_path, "wb") as out_file:
+#                 async for chunk in response.aiter_bytes():
+#                     # if await request.is_disconnected():
+#                     #     print("客户端断开连接，清理未完成的文件")
+#                     #     await out_file.close()
+#                     #     os.remove(file_path)
+#                     #     return False
+#                     await out_file.write(chunk)
+#             return True
+
+
+def download_video(url_desc, headers: dict = None, file_path: str = None):
+    # if not os.path.exists(file_path):
+    # os.makedirs(file_path)
+
+    # for idx, (video_url, caption) in enumerate(videos):
+    video_url = url_desc[0]
+    caption = url_desc[1]
     headers = (
         {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -76,44 +113,97 @@ async def fetch_data_stream(url: str, headers: dict = None, file_path: str = Non
         if headers is None
         else headers.get("headers")
     )
-    async with httpx.AsyncClient() as client:
-        # 启用流式请求
-        print(f"流式下载url: {url}")
-        async with client.stream("GET", url, headers=headers) as response:
-            response.raise_for_status()
+    for i in range(len(url_desc)):
+        try:
+            if str(caption).startswith("."):
+                # 跳过 .开头
+                continue
+            if len(str(caption).strip().replace(".", "").strip()) == 0:
+                # 跳过空文本视频
+                print("skip empty caption video")
+                continue
+            caption = str(caption).replace("*", "")
 
-            # 流式保存文件
-            async with aiofiles.open(file_path, "wb") as out_file:
-                async for chunk in response.aiter_bytes():
-                    # if await request.is_disconnected():
-                    #     print("客户端断开连接，清理未完成的文件")
-                    #     await out_file.close()
-                    #     os.remove(file_path)
-                    #     return False
-                    await out_file.write(chunk)
-            return True
+            print(f"Downloading video : {caption}")
+            caption = str(caption).replace("\n", "")
+            video_filename = os.path.join(file_path, f"{caption}.mp4")
+            if os.path.exists(video_filename):
+                print(f"Video {video_filename} already exists, skipping...")
+                continue
+
+            response = requests.get(video_url, headers=headers)
+            with open(video_filename, "wb") as f:
+                f.write(response.content)
+        except Exception as e:
+            print(f"Error downloading video  {e}")
 
 
-async def download_tiktok_urls(urls: str):
-    # 开始解析数据/Start parsing data
+async def download_single_url(url_desc: tuple[str, str]):
+    try:
+        # 开始解析数据
+        # try:
+        #     data = await HybridCrawler().hybrid_parsing_single_video(
+        #         url=url, minimal=True
+        #     )
+        # except Exception as e:
+        #     print("hybrid_parsing_single_video 报错: ", e)
+        #     return
+
+        # description = data.get("desc").strip()
+        # if len(description) > 1096:
+        #     description = str(description.strip())[:1096]
+
+        no_watermark_video_url = url_desc[0]
+        description = url_desc[1]
+        file_path = os.path.abspath(
+            os.path.join("downloads_tiktok", description + ".mp4")
+        )
+        if os.path.exists(file_path):
+            print(f"已存在, 跳过: {no_watermark_video_url}")
+            return
+        __headers = await HybridCrawler().TikTokWebCrawler.get_tiktok_headers()
+        # no_watermark_video_url = data.get("video_data").get("nwm_video_url_HQ")
+        success = download_video(
+            no_watermark_video_url, headers=__headers, file_path=file_path
+        )
+        if not success:
+            print(f"下载失败: {no_watermark_video_url}")
+    except Exception as e:
+        print("error: ", e)
+
+
+async def parse_tiktok_urls(urls: List[str]):
+    # 开始解析数据
+    ret_data = []
     for url in urls:
         try:
-            data = await HybridCrawler().hybrid_parsing_single_video(url=url, minimal=True)
-
-            description = data.get("desc")
-            file_path = os.path.abspath(os.path.join("downloads_tiktok", description + ".mp4"))
-            __headers = await HybridCrawler().TikTokWebCrawler.get_tiktok_headers()
-            no_watermark_video_url = data.get("video_data").get("nwm_video_url_HQ")
-            success = await fetch_data_stream(
-                no_watermark_video_url, headers=__headers, file_path=file_path
+            data = await HybridCrawler().hybrid_parsing_single_video(
+                url=url, minimal=True
             )
-            if not success:
-                print(f"下载失败: {url}")
+            no_watermark_video_url = data.get("video_data").get("nwm_video_url_HQ")
+            description = data.get("desc").strip()
+            if len(description) > 200:
+                description = str(description.strip())[:200]
+            description = description.replace("/", "_")
+            ret_data.append((no_watermark_video_url, description))
         except Exception as e:
-            code = 400
-            print("error: ", e)
-            return
-    return
+            print("hybrid_parsing_single_video 报错: ", e)
+            # return
+    return ret_data
+
+
+def download_url_wrapper(url_desc):
+    # 在单独的进程中运行异步函数
+    asyncio.run(download_single_url(url_desc))
+
+
+def download_tiktok_urls(url_descs: list):
+    # 创建下载目录
+    os.makedirs("downloads_tiktok", exist_ok=True)
+
+    # 使用多进程池
+    with Pool(cpu_count() - 1) as pool:
+        pool.map(download_url_wrapper, url_descs)
 
 
 # 获取页面内容
@@ -142,8 +232,8 @@ async def get_page_videos(url):
             ret_videos.update(r)
             tmp_start = time.time()
 
-            # TODO:
-            await download_tiktok_urls(r)
+            url_descs = await parse_tiktok_urls(r)
+            download_tiktok_urls(url_descs)
             print("==================")
             print(r)
             print("==================")
@@ -198,7 +288,9 @@ def get_video_share_url():
         try:
             link = video_div.find_element(By.TAG_NAME, "a")
             video_share_url = link.get_attribute("href")
-            videos.append(video_share_url)
+            if video_share_url not in url_sets:
+                videos.append(video_share_url)
+                url_sets.add(video_share_url)
         except NoSuchElementException as e:
             print("video标签不存在: " + str(e))
         except Exception as e:
@@ -219,7 +311,40 @@ async def main():
 
     # login_tiktok()
 
-    pages = ["https://www.tiktok.com/tag/meme"]
+    pages = [
+        # "https://www.tiktok.com/tag/meme",
+        # "https://www.tiktok.com/tag/funny",
+        "https://www.tiktok.com/tag/food",
+        "https://www.tiktok.com/tag/dancing",
+        "https://www.tiktok.com/tag/dance",
+        "https://www.tiktok.com/tag/eating",
+        "https://www.tiktok.com/tag/fyp",
+        "https://www.tiktok.com/tag/foryou",
+        "https://www.tiktok.com/tag/foryoupage",
+        "https://www.tiktok.com/tag/funnyvideos",
+        "https://www.tiktok.com/tag/fy",
+        "https://www.tiktok.com/tag/prank",
+        "https://www.tiktok.com/tag/fun",
+        "https://www.tiktok.com/tag/pets",
+        "https://www.tiktok.com/tag/anime",
+        "https://www.tiktok.com/tag/beauty",
+        "https://www.tiktok.com/tag/basketball",
+        "https://www.tiktok.com/tag/blackpink",
+        "https://www.tiktok.com/tag/cute",
+        "https://www.tiktok.com/tag/capcut",
+        "https://www.tiktok.com/tag/comedia",
+        "https://www.tiktok.com/tag/duet",
+        "https://www.tiktok.com/tag/explore",
+        "https://www.tiktok.com/tag/game",
+        "https://www.tiktok.com/tag/humor",
+        "https://www.tiktok.com/tag/happy",
+        "https://www.tiktok.com/tag/kpop",
+        "https://www.tiktok.com/tag/makeup",
+        "https://www.tiktok.com/tag/music",
+        "https://www.tiktok.com/tag/love",
+        "https://www.tiktok.com/tag/like",
+        "https://www.tiktok.com/tag/new",
+    ]
     for url in pages:
         try:
             await get_page_videos(url)
